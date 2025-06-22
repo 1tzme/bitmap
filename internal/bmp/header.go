39fa4238dd3d344a541ec6e50a0f906f5593ecde
1tzme <zakir.itsme@gmail.com>
2025-06-22 18:05:56 +0500

    refactor: formatted error

--- internal/bmp/header.go
+++ internal/bmp/header.go
@@ -41,7 +41,7 @@ func printHeader(path string) {
 func ReadHeader(path string) *Header {
 	file, err := os.Open(path)
 	if err != nil {
-		log.Fatalln("Failed to open file: ", err)
+		log.Fatalf("Failed to open file: %v", err)
 	}
 	defer file.Close()
 
@@ -51,46 +51,46 @@ func ReadHeader(path string) *Header {
 	fileType := make([]byte, 2)
 	_, err = file.Read(fileType)
 	if err != nil {
-		log.Fatal("Failed to read file type: ", err)
+		log.Fatalf("Failed to read file type: %v", err)
 	}
 	header.FileType = string(fileType)
 	if header.FileType != "BM" {
-		log.Fatal(path, " is not a valid BMP file")
+		log.Fatalf(path, " is not a valid BMP file")
 	}
 
 	err = binary.Read(file, binary.LittleEndian, &header.FileSize)
 	if err != nil {
-		log.Fatal("Failed to read file size", err)
+		log.Fatalf("Failed to read file size: %v", err)
 	}
 
 	_, err = file.Seek(8, io.SeekCurrent)
 	if err != nil {
-		log.Fatal("Failed to skip reserved and offset fields: ", err)
+		log.Fatalf("Failed to skip reserved and offset fields: %v", err)
 	}
 
 	err = binary.Read(file, binary.LittleEndian, &header.DibHeaderSize)
 	if err != nil {
-		log.Fatal("Failed to read DIB header size: ", err)
+		log.Fatalf("Failed to read DIB header size: %v", err)
 	}
 
 	err = binary.Read(file, binary.LittleEndian, &header.WidthInPixels)
 	if err != nil {
-		log.Fatal("Failed to read width: ", err)
+		log.Fatalf("Failed to read width: %v", err)
 	}
 
 	err = binary.Read(file, binary.LittleEndian, &header.HeightInPixels)
 	if err != nil {
-		log.Fatal("Failed to read height: ", err)
+		log.Fatalf("Failed to read height: %v", err)
 	}
 
 	_, err = file.Seek(2, io.SeekCurrent)
 	if err != nil {
-		log.Fatal("Failed to skip planes: ", err)
+		log.Fatalf("Failed to skip planes: %v", err)
 	}
 
 	err = binary.Read(file, binary.LittleEndian, &header.PixelSize)
 	if err != nil {
-		log.Fatal("Failed to read pixel size: ", err)
+		log.Fatalf("Failed to read pixel size: %v", err)
 	}
 	if header.PixelSize != 24 {
 		log.Fatal("Only 24 bit BMP files are allowed")
@@ -98,17 +98,17 @@ func ReadHeader(path string) *Header {
 
 	_, err = file.Seek(4, io.SeekCurrent)
 	if err != nil {
-		log.Fatal("Failed to skip compression: ", err)
+		log.Fatalf("Failed to skip compression: %v", err)
 	}
 
 	err = binary.Read(file, binary.LittleEndian, &header.ImageSize)
 	if err != nil {
-		log.Fatal("Failed to read image size: ", err)
+		log.Fatalf("Failed to read image size: %v", err)
 	}
 
 	_, err = file.Seek(16, io.SeekCurrent)
 	if err != nil {
-		log.Fatal("Failed to skip remaining fields: ", err)
+		log.Fatalf("Failed to skip remaining fields: %v", err)
 	}
 
 	return header
