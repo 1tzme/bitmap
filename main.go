e0d78b42c06e34a90a7ee23681705f17ab3062bc
1tzme <zakir.itsme@gmail.com>
2025-06-22 12:43:20 +0500

    chore: add usage, sample

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	u "bitmap/internal/utils"
+)
+
 func main() {
-	
+	if len(os.Args) < 2 {
+		u.PrintUsage()
+		os.Exit(1)
+	}
+	switch os.Args[1] {
+	default:
+		u.PrintUsage()
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", os.Args[1])
+		os.Exit(1)
+	}
 }
